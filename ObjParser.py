2a1596d9787c3e114bbc936112517d4650200362
Furkan Emre <furkan54emre@gmail.com>
2019-04-04 19:20:27 +0300

    Assignment 3 Starts

---
+++ ObjParser.py
@@ -0,0 +1,16 @@
+# CENG 487 Assignment2 by
+# Furkan Emre Yilmaz
+# StudentId: 230201057
+# April 2019
+
+class ObjParser:
+
+    def __init__(self):
+        print("ObjParse")
+
+
+
+
+
+if __name__ == "__main__":
+    a = ObjParser()
