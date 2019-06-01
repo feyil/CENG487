2d552075cd8c447b6629e672a2e05a833ea01577
Furkan Emre <furkan54emre@gmail.com>
2019-06-01 12:02:49 +0300

    Input Checking Added

--- assignment3.py
+++ assignment3.py
@@ -18,6 +18,11 @@ from Cylinder import Cylinder
 from Box import Box
 from Scene import *
 
+# input checking
+if(len(sys.argv) != 2):
+	print("\nPlease provide .obj file:\n  >python assignment4.py *.obj\n")
+	exit()
+
 # Second camera pitch and yaw movement can jump if we rotate using arrow key 360 degree we can use 
 # both camera without any problem. I am looking for the cause of the jump for "cam2"
 
